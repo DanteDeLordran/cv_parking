1d67c226e8036e17a4193dd90a67de062aee819d
Lordran <lordran@lordran.dev>
2024-06-27 00:44:23 -0600

    Refactored main.py

---
+++ app/services/services.py
@@ -0,0 +1,25 @@
+from sqlmodel import Session, select
+from ..models.models import Employee
+from ..db.database import engine
+
+
+def get_all_employees_car_registry():
+    with Session(engine) as session:
+        employees = session.exec(select(Employee))
+
+        if len(employees.all()) == 0:
+            print(f"No registry {len(employees.all())}")
+        else:
+            for employee in employees:
+                print(employee.car_registry)
+
+
+def get_employee_by_car_registry(registry: str):
+    with Session(engine) as session:
+        employee = session.exec(select(Employee).where(Employee.car_registry == registry)).first()
+        if employee is not None:
+            print(f"{employee.name} {employee.lastname}")
+            return True
+        else:
+            print(f"No employee with car registry {registry}")
+            return False
